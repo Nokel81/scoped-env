3c07240365a435c78df216054876a832de53d70b
Bart Massey <bart.massey@gmail.com>
2020-01-31 15:13:00 -0800

    major cleanups and API changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,44 @@
-#![crate_name = "scoped_env"]
+use std::env;
+use std::ffi::{OsStr, OsString};
 
-use std::{
-    env::{self, VarError},
-    ffi::{OsStr, OsString},
-};
-
-/// A rust lifetime scope for a set environment variable. When an instance goes out of scope
-/// it will automatically cleanup the environment
+/// A rust lifetime scope for a set environment
+/// variable. When an instance goes out of scope it will
+/// automatically cleanup the environment
 pub struct ScopedEnv<T>
 where
     T: AsRef<OsStr>,
 {
     name: T,
+    old_value: Option<OsString>,
 }
 
 impl<T> ScopedEnv<T>
 where
     T: AsRef<OsStr>,
 {
-    /// Sets the environment variable {name} to {value}. The returned instance should be assigned
-    /// to a `_name` binding so that it lasts as long as the current block.
+    /// Sets the environment variable {name} to {value}. The
+    /// returned instance should be assigned to a `_name`
+    /// binding so that it lasts as long as the current
+    /// block.
     ///
     /// ```rust
     /// use scoped_env::ScopedEnv;
     /// let c = ScopedEnv::set("HELLO", "WORLD");
-    /// assert_eq!(c.get().unwrap().as_str(), "WORLD");
+    /// assert_eq!(std::env::var(c).unwrap().as_str(), "WORLD");
     /// ```
     pub fn set(name: T, value: T) -> Self {
-        env::set_var(&name, value);
-        Self { name }
+        let old_value = env::var_os(name.as_ref());
+        env::set_var(name.as_ref(), value);
+        Self { name, old_value }
     }
+}
 
-    /// Used to get the stored value of the assosiated name
-    pub fn get_os(&self) -> Option<OsString> {
-        env::var_os(&self.name)
-    }
-
-    // Used to get the stored value of the assosiated name and convert it to a rust `String`
-    pub fn get(&self) -> Result<String, VarError> {
-        env::var(&self.name)
+impl<T> AsRef<OsStr> for ScopedEnv<T>
+where
+    T: AsRef<OsStr>,
+{
+    fn as_ref(&self) -> &OsStr {
+        self.name.as_ref()
     }
 }
 
@@ -47,7 +47,12 @@ where
     T: AsRef<OsStr>,
 {
     fn drop(&mut self) {
-        env::remove_var(&self.name);
+        match self.old_value {
+            Some(ref old_value) => {
+                env::set_var(self.as_ref(), old_value)
+            }
+            None => env::remove_var(self),
+        }
     }
 }
 
@@ -57,17 +62,29 @@ mod tests {
 
     #[test]
     fn does_set() {
-        let _c = ScopedEnv::set("FOOBAR", "hello");
-        assert_eq!(env::var_os("FOOBAR").unwrap().to_str().unwrap(), "hello");
+        let c = ScopedEnv::set("FOOBAR", "hello");
+        assert_eq!(env::var(c).unwrap(), "hello");
     }
 
     #[test]
     fn does_unset_at_end_of_block() {
+        env::remove_var("FOOBAR1");
         {
-            let _c = ScopedEnv::set("FOOBAR1", "hello");
-            assert_eq!(env::var_os("FOOBAR1").unwrap().to_str().unwrap(), "hello");
+            let c = ScopedEnv::set("FOOBAR1", "hello");
+            assert_eq!(env::var(c).unwrap(), "hello");
         }
 
         assert_eq!(env::var_os("FOOBAR1"), None);
+    }
+
+    #[test]
+    fn does_reset_at_end_of_block() {
+        env::set_var("FOOBAR1", "OLD_VALUE");
+        {
+            let c = ScopedEnv::set("FOOBAR1", "hello");
+            assert_eq!(env::var(c).unwrap(), "hello");
+        }
+
+        assert_eq!(env::var("FOOBAR1").unwrap(), "OLD_VALUE");
     }
 }
